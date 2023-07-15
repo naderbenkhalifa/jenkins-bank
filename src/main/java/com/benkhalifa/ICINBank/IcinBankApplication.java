bda6d61cc05bac496356a7354ece6f21b4cd531a
naderbenkhalifa <nader.benkhalifa78@gmail.com>
2023-07-15 17:13:47 +0100

    new version

--- src/main/java/com/benkhalifa/ICINBank/IcinBankApplication.java
+++ src/main/java/com/benkhalifa/ICINBank/IcinBankApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class IcinBankApplication {
 	
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	  public String home() {
 	    return "Hello Docker world";
 	  }
